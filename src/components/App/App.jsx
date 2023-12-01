0487d02e2d64f2e2bf8f24f0abd3ea45b4ae976f
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-12-01 13:53:26 +0200

    edited

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,10 @@ const App = () => {
 
   const [contacts, setContacts] = useState(() => {
     const getContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(getContacts) ?? contacts;
+    const parsedContacts = JSON.parse(getContacts) ?? [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },];
 
     return parsedContacts;
   }, []);
